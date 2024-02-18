597b0580d92c192572b4bea0abfe3e9db8fe0605
Andrey Shevchenko <vakarianx@yandex.ru>
2024-02-18 19:14:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp15/Poetry.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Poetry
+    {
+        private static object o = new object();
+        private static int numberOfString = 0;
+        static int delay = 0;
+        private string StringFromPoetry(int numberOfString)
+        {
+            // Для экономии времени не стал помещать все в файл
+            string poetry = "#Буря мглою небо кроет,#" +
+                "Вихри снежные крутя;#" +
+                "То, как зверь, она завоет,#" +
+                "то заплачет как дитя,";
+            string[] poetryStrings = poetry.Split('#');
+            return poetryStrings[numberOfString];
+        }
+
+        public string StringTask()
+        {            
+            lock (this)
+            {
+                Random rnd = new Random();
+                if (delay == 0) delay = rnd.Next(1000, 4000);
+                else delay += rnd.Next(-2000, 2000);
+                Thread.Sleep(100);
+                Console.WriteLine($"Поток {Task.CurrentId} начал работу. Задержка {delay}");
+            }
+
+            Thread.Sleep(delay);
+
+            lock (this)
+            {
+                numberOfString++;
+                return StringFromPoetry(numberOfString);
+            }            
+        }
+    }
+}
---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Poetry poetry = new Poetry();
+           
+            Task<string> task1 = Task.Run(poetry.StringTask);
+            StringTask(task1);
+            Task<string> task2 = Task.Run(poetry.StringTask);
+            StringTask(task2);
+            Task<string> task3 = Task.Run(poetry.StringTask);
+            StringTask(task3);
+            Task<string> task4 = Task.Run(poetry.StringTask);
+            StringTask(task4);
+
+            Console.ReadKey();
+        }
+
+        private async static void StringTask(Task<string> res)
+        {           
+            await res;
+            PrintString($"{res.Result} <- выполнил поток {res.Id}");
+        }
+
+        private static void PrintString(string data)
+        {
+            Console.WriteLine(data);
+        }
+    }
+}
